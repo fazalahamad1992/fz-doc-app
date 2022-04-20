d663127f927646b405cc6f144085a398933f9fda
fazal.mohammad <fazal.mohammad@servicenow.com>
2022-04-21 01:05:08 +0530

    Added basic folder structure for DocApp.

---
+++ doc-app/src/test/java/com/fz/docapp/DocAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fz.docapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
